379df3a102a4b94e7524d2a32200efb6559287b3
Frederik B <fbraun+gh@mozilla.com>
2014-09-25 15:22:15 +0200

    set parser in mocha-include

--- tests/cases/production_ruletests.js
+++ tests/cases/production_ruletests.js
@@ -1,19 +1,15 @@
 describe('Testing production rules (common/rules.json)', function () {
-  $.ajax({
-    url: '../common/rules.json',
-    async: false,
-    dataType: 'json'
-  }).done(function (ruleData) {
+  function testSetup(ruleData) {
 
     ruleData.forEach(function (rule) {
       describe('Rule: ' + rule.name, function () {
 
         rule.testhit.split(";").forEach(function (testsplit) {
-          if(testsplit.trim()!=""){
+          if (testsplit.trim() != "") {
             it(rule.source + " should match " + testsplit, function () {
               ScanJS.loadRules([rule]);
-              var parsedTest = acorn.parse(testsplit, {locations: true});
-              var results = ScanJS.scan(parsedTest);
+              var ast = acorn.parse(testsplit, {locations: true});
+              var results = ScanJS.scan(ast);
               chai.expect(results.length).to.equal(1);
             });
           }
@@ -21,11 +17,23 @@ describe('Testing production rules (common/rules.json)', function () {
 
         it(rule.name + " should not match " + rule.testmiss, function () {
           ScanJS.loadRules([rule]);
-          var parsedTest = acorn.parse(rule.testmiss, {locations: true});
-          var results = ScanJS.scan(parsedTest);
+          var ast = acorn.parse(rule.testmiss, {locations: true});
+          var results = ScanJS.scan(ast);
           chai.expect(results).to.have.length(0);
         });
       });
     });
-  });
+  }
+  if (typeof $ !== "undefined") {
+    $.ajax({
+      url: '../common/rules.json',
+      async: false,
+      dataType: 'json'
+    }).done(testSetup);
+  }
+  else if (typeof require !== "undefined") {
+    var fs = require("fs");
+    var signatures = JSON.parse(fs.readFileSync("../common/rules.json", "utf8"));
+    testSetup(signatures);
+  }
 })
--- tests/cases/test_ruletests.js
+++ tests/cases/test_ruletests.js
@@ -1,14 +1,10 @@
 describe('Testing rule templates (common/template_rules.json)', function () {
-  $.ajax({
-    url: '../common/template_rules.json',
-    async: false,
-    dataType: 'json'
-  }).done(function (ruleData) {
+  function testSetup(ruleData) {
 
     ruleData.forEach(function (rule) {
       describe('Rule: ' + rule.name, function () {
         it(rule.name + " should match template " + rule.name, function () {
-          var template=ScanJS.parseRule(rule);
+          var template = ScanJS.parseRule(rule);
           chai.expect(rule.name).to.equal(template);
         });
 
@@ -16,7 +12,8 @@ describe('Testing rule templates (common/template_rules.json)', function () {
           if(testsplit.trim()!=""){
             it(rule.source + " should match " + testsplit, function () {
               ScanJS.loadRules([rule]);
-              var results = ScanJS.scan(acorn.parse(testsplit, {locations:true}));
+              var ast = acorn.parse(testsplit, {locations: true});
+              var results = ScanJS.scan(ast);
               chai.expect(results.length).to.equal(1);
             });
           }
@@ -24,10 +21,23 @@ describe('Testing rule templates (common/template_rules.json)', function () {
 
         it(rule.name + " should not match " + rule.testmiss, function () {
           ScanJS.loadRules([rule]);
-          var results = ScanJS.scan(acorn.parse(rule.testmiss, {locations:true}));
+          var ast = acorn.parse(rule.testmiss, {locations: true});
+          var results = ScanJS.scan(ast);
           chai.expect(results).to.have.length(0);
         });
       });
     });
-  });
+  }
+  if (typeof $ !== "undefined") {
+    $.ajax({
+      url: '../common/template_rules.json',
+      async: false,
+      dataType: 'json'
+    }).done(testSetup);
+  }
+  else if (typeof require !== "undefined") {
+    var fs = require("fs");
+    var signatures = JSON.parse(fs.readFileSync("../common/template_rules.json", "utf8"));
+    testSetup(signatures);
+  }
 })
