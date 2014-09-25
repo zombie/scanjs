2d40c16a166d29770ea2a59826c1ee3a4c7c042c
Frederik B <fbraun+gh@mozilla.com>
2014-09-25 14:39:49 +0200

    adding first stab at mocha includes

---
+++ tests/mocha-includes.js
@@ -0,0 +1,11 @@
+global.ScanJS = require("../common/scan.js");
+var fs = require("fs");
+var signatures = JSON.parse(fs.readFileSync("../common/rules.json", "utf8"));
+ScanJS.loadRules(signatures);
+
+global.acorn = require("acorn");
+global.chai = require("chai");
+global.assert = chai.assert;
+global.should = require("chai").should();
+global.expect = require("chai").expect;
+global.AssertionError = require("chai").AssertionError;
